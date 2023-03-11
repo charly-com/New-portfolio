b6ab495af3208a53eb981df983177f4f5a96a4f5
Decagon <uchecharles223@gmail.com>
2023-03-11 16:17:55 +0100

    worked on the Expreience, Home and SocialLink in the componenet folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/Nav-bar";
+import Home from "./components/Home";
+import Experience from "./components/Experience";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
      <Navbar />
+     <Home />
+     <Experience />
+     <SocialLinks />
     </div>
   );
 }
